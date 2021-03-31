37a23ef75707a0d2a40b192be808275e910293d0
hiroshima820 <53898556+hiroshima820@users.noreply.github.com>
2021-03-31 13:07:08 +0900

    Mod

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,12 +9,4 @@ environment.plugins.prepend(
   })
 );
 
-environment.config.delete("node.dgram");
-environment.config.delete("node.fs");
-environment.config.delete("node.net");
-environment.config.delete("node.tls");
-environment.config.delete("node.child_process");
-
-environment.config.merge(customConfig);
-
 module.exports = environment;
