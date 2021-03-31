d27f8961f5ece3eb759fb940bbfd046eb67e9b67
hiroshima820 <53898556+hiroshima820@users.noreply.github.com>
2021-03-31 13:04:27 +0900

    Add environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -9,4 +9,12 @@ environment.plugins.prepend(
   })
 );
 
+environment.config.delete("node.dgram");
+environment.config.delete("node.fs");
+environment.config.delete("node.net");
+environment.config.delete("node.tls");
+environment.config.delete("node.child_process");
+
+environment.config.merge(customConfig);
+
 module.exports = environment;
